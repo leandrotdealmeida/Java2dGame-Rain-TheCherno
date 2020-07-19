bd92dc4feb7500f091d894ae81119e55bd48d343
Leandro Almeida <leandrotrovilho@gmail.com>
2020-07-18 21:31:22 -0300

    Create Void Tile

--- src/com/thecherno/rain/graphics/Sprite.java
+++ src/com/thecherno/rain/graphics/Sprite.java
@@ -8,6 +8,7 @@ public class Sprite {
 	private SpriteSheet sheet;
 	
 	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);
+	public static Sprite voidSprite = new Sprite(16, 0x1B87E0);
 	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		this.SIZE = size;
@@ -19,6 +20,19 @@ public class Sprite {
 		
 	}
 	
+	public Sprite(int size, int colour) {
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		setColour(colour);
+	}
+	
+	private void setColour(int colour) {
+		for(int i = 0; i < SIZE * SIZE; i++) {
+			pixels[i] = colour;
+		}
+		
+	}
+
 	public void load() {
 		for (int y = 0; y < SIZE; y++) {
 			for (int x = 0; x < SIZE; x++) {
--- src/com/thecherno/rain/level/tile/VoidTile.java
+++ src/com/thecherno/rain/level/tile/VoidTile.java
@@ -3,18 +3,15 @@ package com.thecherno.rain.level.tile;
 import com.thecherno.rain.graphics.Screen;
 import com.thecherno.rain.graphics.Sprite;
 
-public class GrassTile extends Tile {
+public class VoidTile extends Tile {
 
-	public GrassTile(Sprite sprite) {
-		super(sprite);
-		// TODO Auto-generated constructor stub
+	public VoidTile(Sprite sprite) {
+		super(sprite);		
 	}
-	
-	@Override
+
 	public void render(int x, int y, Screen screen) {
 		screen.renderTile(x, y, this);
+
 	}
-	
-	
 
 }
