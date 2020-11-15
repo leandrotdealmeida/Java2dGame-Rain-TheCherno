b928db46bfd61380fa66c3297152e4f858ce6d38
Leandro Almeida <leandrotrovilho@gmail.com>
2020-11-15 15:51:16 -0300

    Add Level and Screen

--- src/com/thecherno/rain/level/Level.java
+++ src/com/thecherno/rain/level/Level.java
@@ -37,17 +37,21 @@ public class Level {
 	}
 	
 	public void render(int xScrool, int yScroll, Screen screen) {
+		screen.setOffset(xScrool, yScroll);
 		int x0 = xScrool >> 4;
 		int x1 = (xScrool + screen.width) >> 4;
 		int y0 = yScroll >> 4;
 		int y1 = (yScroll + screen.height) >>4;
 		
+		for(int y = y0; y < y1; y++) {
+			for(int x = x0; x < x1; x++) {
+				getTile(x, y).render(x, y, screen);
+			}
+		}		
 	}
 	
 	public Tile getTile(int x, int y) {
 		if(tiles[x + y * width] == 0) return Tile.grass;
-		return Tile.voidTile;
-		
+		return Tile.voidTile;		
 	}
-
 }
